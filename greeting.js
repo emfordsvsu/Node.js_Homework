b15e86d861893165c20f3b78aa2f362152c3e83f
emfordsvsu <emford@svsu.edu>
2020-08-02 22:29:57 -0400

    first commit

---
+++ greeting.js
@@ -0,0 +1,10 @@
+// Create a 'greeting.js' file.
+
+const greeting = function (greeting){}
+
+console.log('greeting');
+
+// 'Greeting.js' file must have module.exports
+
+module.exports = greeting
+
